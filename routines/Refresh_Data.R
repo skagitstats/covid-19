f6b0bcfa5c85da01d45b77e201daf6dfff229866
Marshall Brown <marshalldanielbrown@gmail.com>
2020-03-31 17:57:38 -0700

    update march 31

--- routines/Refresh_Data.R
+++ routines/Refresh_Data.R
@@ -20,7 +20,7 @@ freshdata <- pub %>%
  #html_nodes("tbody") %>% 
   rvest::html_table()  
   
-freshdata$DateStamp = ymd_hms(localTime)
+freshdata$DateStamp = Sys.Date() -1 
 freshdata$News = NA
 
 pastData = read_csv("data/skagit_valley_covid_counts.csv", 
